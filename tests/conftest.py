6a6584c3184df11664eeb40417c8fe458f2cf040
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-07-11 01:23:28 +0300

    Fillin test database

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,10 @@ from sqlalchemy.engine import Engine
 from sqlalchemy import orm
 
 from data.models import Base
-from data.models import User
+from data.models import Expense
 from data.models import Group
+from data.models import Item
+from data.models import User
 from data.db_session import make_alembic_config
 
 
@@ -56,12 +58,29 @@ def session():
 
         _factory = orm.sessionmaker(engine, expire_on_commit=False)
         session: orm.Session = _factory()
-        u1 = User(id=1, first_name='test1')
-        u2 = User(id=2, first_name='test2')
-        u3 = User(id=3, first_name='test3')
-        group = Group()
-        group.users.extend([u1, u2, u3])
-        session.add(group)
+        group1 = Group()
+        item_name = 'Test'
+        user = User(id=0, first_name='test0')
+        item = Item(name=item_name)
+        user.items.append(item)
+        group1.users.append(user)
+
+        for i in range(1, 4):
+            user = User(id=i, first_name=f'test{i}')
+            group1.users.append(user)
+            for _ in range(1, 3):
+                expense = Expense(item_name=item_name, price=100, group_id=group1.id)
+                user.expenses.append(expense)
+
+        group2 = Group()
+        for i in range(4, 7):
+            user = User(id=i, first_name=f'test{i}')
+            group2.users.append(User(id=i, first_name=f'test{i}'))
+            for _ in range(1, 3):
+                expense = Expense(item_name=item_name, price=100, group_id=group2.id)
+                user.expenses.append(expense)
+
+        session.add_all([group1, group2])
         session.commit()
 
         yield session
--- tests/groups_test.py
+++ tests/groups_test.py
@@ -1,3 +1,4 @@
+import pytest
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 
@@ -6,12 +7,19 @@ from services import group_service
 
 
 def test_group_members(session: Session):
-    assert 3 == session.scalar(func.count(User.id))
+    assert 7 == session.scalar(func.count(User.id))
 
 
-def test_members_list(session: Session):
-    group = group_service.get_group_by_id(group_id=1, session=session)
-    assert sorted([u.id for u in group.users]) == [1, 2, 3]
+@pytest.mark.parametrize(
+        'group_id,expected_ids',
+        [
+            (1, [0, 1, 2, 3]),
+            (2, [4, 5, 6]),
+        ],
+    )
+def test_members_list(session: Session, group_id, expected_ids):
+    group = group_service.get_group_by_id(group_id=group_id, session=session)
+    assert sorted([u.id for u in group.users]) == expected_ids
 
 
 def test_not_group(session: Session):
