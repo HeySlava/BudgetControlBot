180c6dd981b486c6aa5aa39aacb57dad2705dfae
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-10-08 01:20:44 +0300

    Echo example config before tests

--- bot/config.py
+++ bot/config.py
@@ -5,21 +5,20 @@ from pathlib import Path
 import pytz
 
 _CONFIG_INI_PATH = Path(__file__).absolute().parent.parent / 'config.ini'
-_default_token = '22:aa-F-6eNoKY65R-omFA'
-_admin_id = 123
 
 configreader = configparser.ConfigParser()
+_error_msg = f'Setup {_CONFIG_INI_PATH} for work. See example.config.ini'
 if not _CONFIG_INI_PATH.exists():
-    token = _default_token
-    admin = _admin_id
-else:
-    configreader.read(_CONFIG_INI_PATH)
-    try:
-        admin = int(configreader['config']['admin'])
-        token = configreader['config']['token']
-    except (KeyError, ValueError):
-        print(f"You didn't setup your {_CONFIG_INI_PATH}. See example.config.ini")
-        raise SystemExit(1)
+    print(_error_msg)
+    raise SystemExit(1)
+
+configreader.read(_CONFIG_INI_PATH)
+try:
+    admin = int(configreader['config']['admin'])
+    token = configreader['config']['token']
+except (KeyError, ValueError):
+    print(_error_msg)
+    raise SystemExit(1)
 
 
 @dataclass
