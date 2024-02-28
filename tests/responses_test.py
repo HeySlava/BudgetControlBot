0e0b2974c04f41189839df00ef9de668516c5c9e
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2024-02-29 00:27:18 +0300

    Fix old tests

--- tests/responses_test.py
+++ tests/responses_test.py
@@ -1,11 +1,11 @@
 from handlers._responses import RESPONSES
 
 
-def current_balance_test():
+def test_current_balance():
     expected = (
             'Изменить баланс на ЧИСЛО - /balance ЧИСЛО. '
             'Поддерживаются целые числа, а так же выражения с ними. '
-            'Например: "100 + 200", или "100 '
+            'Например: "100 + 200", или "100 -200".'
             '\n'
             '* все что ты напишешь с новой строки строки будет '
             'расцениваться как комментарий и будет сохранен в историю'
@@ -17,16 +17,15 @@ def current_balance_test():
     assert expected == msg
 
 
-def write_expence_test():
+def test_write_expence():
     expected = (
             "Впиши свой расход для 'test item'. "
             'Поддерживаются целые числа, а так же выражения с ними. '
-            'Например: "100 + 200", или "100 '
+            'Например: "100 + 200", или "100 -200".'
             '\n'
             '* все что ты напишешь с новой строки строки будет '
             'расцениваться как комментарий и будет сохранен в историю'
-            '\n\n'
         )
-    msg = RESPONSES['current_balance'].format(item='test item')
+    msg = RESPONSES['write_expence'].format(item='test item')
 
     assert expected == msg
